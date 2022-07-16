f929a9e33a583e4be7688de7478ff62803f30224
paolahernandezrobles <paolahernandezrobles2aqgmail.com>
2022-07-16 15:17:07 -0500

    16 de julio

--- js/Java.js
+++ js/Java.js
@@ -1,4 +1,8 @@
-function cambiar_Biodivercidad(
- f
-)
-    
+const btnswitch = document.querySelector("#switch")
+
+btnswitch.addEventListener("click", () =>{
+    document.body.classList.toggle('dark');
+    btnswitch.classList.toggle('active');
+    document.header.classList.toggle('dark')
+
+})
